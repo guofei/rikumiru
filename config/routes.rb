f399ed900fca7cd842bb4dbecb296bc1845e7681
guofei <guofei1986@gmail.com>
2014-08-01 21:55:20 +0900

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   resources :companies
 
   resources :tweets
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
