fcc5cf51b3812f782c6ee1ff7d7ea4a3fb339027
guofei <guofei1986@gmail.com>
2014-08-06 00:53:38 +0900

    change countcache

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,11 @@
 class Keyword < ActiveRecord::Base
   has_and_belongs_to_many :tweets, join_table: :tweets_keywords
   before_destroy { tweets.clear }
+
+  def self.reset_tweet_count
+    self.all.each do |k|
+      count = k.tweets.where(useful: true).count
+      k.update_attributes(:tweet_count => count)
+    end
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,5 +2,22 @@ class Tweet < ActiveRecord::Base
   belongs_to :company
   has_and_belongs_to_many :keywords, join_table: :tweets_keywords
   default_scope { order("created_at desc") }
-  before_destroy { keywords.clear }
+  before_destroy do
+    keywords.clear
+    decrement_tweet_count
+  end
+
+  before_save :increment_tweet_count
+
+  def increment_tweet_count
+    self.keywords.each do |k|
+      k.increment!(:tweet_count)
+    end
+  end
+
+  def decrement_tweet_count
+    self.keywords.each do |k|
+      k.decrement!(:tweet_count)
+    end
+  end
 end
