9b11bf6c7b6f6575612e3efe5bd5ef5a7994bf06
guofei <guofei1986@gmail.com>
2014-08-07 00:36:38 +0900

    sorted by tweet

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@ class Company < ActiveRecord::Base
   has_many :tweets
 
   scope :sorted_by_name, -> { where("id < 201").order("name") }
+  scope :sorted_by_tweet, -> { joins(:tweets).group("tweets.company_id").order("count(tweets.company_id) desc") }
 end
