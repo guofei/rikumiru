9c692a2c3676f3fda814b127dd81ce52449ec704
guofei <guofei1986@gmail.com>
2014-08-07 00:26:24 +0900

    add companies

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     @tweets = Tweet.where(useful: true).take(100)
     @keywords = Keyword.rank.take(20)
+    @companies = Company.take(20)
   end
 end
