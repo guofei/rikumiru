4281ab25bc08a6e466b9e2d47640516da15d6738
guofei <guofei1986@gmail.com>
2014-08-07 00:31:19 +0900

    top company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
   has_many :tweets
 
-  scope :sorted_by_name, -> { where("id < 501").order("name") }
+  scope :sorted_by_name, -> { where("id < 201").order("name") }
 end
