535eb449139858e9c25c8e967858ba7305c58378
guofei <guofei1986@gmail.com>
2014-08-07 00:32:32 +0900

    change top

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     @tweets = Tweet.where(useful: true).take(100)
     @keywords = Keyword.rank.take(20)
-    @companies = Company.sorted_by_name.take(20)
+    @companies = Company.first(100)
   end
 end
