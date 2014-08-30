8bc262a888b3084ca68348df0c7a58274d17a7fb
guofei <guofei1986@gmail.com>
2014-08-30 22:18:10 +0900

    ad comment admin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:destroy]
+  before_action :admin_check, only: [:destroy]
+
   def create
     comment = Comment.new comment_params
     commentable = params[:comment][:commentable_type].constantize.find(params[:comment][:commentable_id])
@@ -6,6 +9,19 @@ class CommentsController < ApplicationController
     comment.user = current_user
     comment.save
     redirect_to commentable
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    redirect_to companies_url and return unless @comment.commentable_type == "Company"
+
+    @company = @comment.commentable
+    @comment.destroy
+    respond_to do |format|
+      format.js
+      format.html { redirect_to company_url(@company), notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
