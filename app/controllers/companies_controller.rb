e31740cfc405a34df0fd9c0e9b7eef849cde049c
guofei <guofei1986@gmail.com>
2014-09-07 15:04:35 +0900

    fix link

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,7 +6,7 @@ class CompaniesController < ApplicationController
   # GET /companies
   # GET /companies.json
   def index
-    @companies = Company.rank.page params[:page]
+    @companies = Company.rank.where("tweet_count > 0").page params[:page]
   end
 
   # GET /companies/1
