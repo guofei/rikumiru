e12c9a78f12fb7b1ab45a809e9bb08f0486ef50f
guofei <guofei1986@gmail.com>
2014-09-14 16:34:46 +0900

    remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,14 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 
 //= require bootstrap-sprockets
-//= require google-analytics-turbolinks
 //= require highcharts
 //= require chartkick
 
 //= require_tree .
-
-//= require turbolinks
