5e54e10a924953fe6f01bf6b5b7ba309808c5d5f
guofei <guofei1986@gmail.com>
2014-09-15 21:10:45 +0900

    add redis cache to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,15 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  config.cache_store = :memory_store
+  config.cache_store = :redis_store, {
+    :host => "localhost",
+    :port => 6379,
+    :db => 0,
+    :password => Rails.application.secrets.redis_password,
+    :namespace => "rikulibcache",
+    :expires_in => 24.hours
+  }
+
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
