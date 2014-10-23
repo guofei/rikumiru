9e0759eb61f2de5d2abef8b9e6bacd093e95cc50
guofei <guofei1986@gmail.com>
2014-10-23 22:29:30 +0900

    change search to google

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,11 @@
 class SearchesController < ApplicationController
   def index
-    @companies = Company.search(name_cont: params[:company]).result
-    if @companies.count == 1
-      redirect_to company_path(@companies.first) and return
-    end
+    keywords = params[:company] || ''
+    keywords.gsub!('#', '%23')
+    redirect_to "https://www.google.co.jp/#q=site:www.rikulib.com+#{keywords}"
+    # @companies = Company.search(name_cont: params[:company]).result
+    # if @companies.count == 1
+    #   redirect_to company_path(@companies.first) and return
+    # end
   end
 end
