aab3e56ada3a3c1b3d99092c37269ae4d70d4974
guofei <guofei1986@gmail.com>
2015-06-05 12:01:23 +0900

    fix image error

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,5 @@
 module TweetsHelper
+  def img_url(tweet)
+    tweet.profile_img.nil? ? "http://abs.twimg.com/sticky/default_profile_images/default_profile_2_normal.png" : tweet.profile_img
+  end
 end
