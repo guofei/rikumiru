5ee4186bf9dda18c1f3e1aeea3fe557cb484dbf4
guofei <guofei1986@gmail.com>
2015-06-18 23:18:37 +0900

    add minlen

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,6 +21,7 @@ class TweetsController < ApplicationController
     @tweets = @tweets.where("company_id <= #{params[:company].to_i}") if params[:company]
     @tweets = @tweets.where("text like '%#{params[:keyword]}%'") if params[:keyword]
     @tweets = @tweets.where("length(text) < ?", params[:maxlen].to_i) if params[:maxlen]
+    @tweets = @tweets.where("length(text) >= ?", params[:minlen].to_i) if params[:minlen]
 
     @count = @tweets.count
     @tweets = @tweets.includes(:company).page params[:page]
